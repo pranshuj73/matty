6a26ecf5b8746714fad1cdabcd57854281cabf49
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-07-14 05:20:35 +0530

    fix(navbar): change position to fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Hi from "@/components/hi";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className + " dark"}>
+      <body className={cn(inter.className, "dark")}>
         <Hi />
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/landing/Hero";
 import Navbar from "@/components/landing/Navbar";
+import { BGEffect } from "@/components/ui/bg-anim";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="py-12 px-8">
+    <main className="flex flex-col items-center py-12 px-8">
       <Navbar />
       <Hero />
 
