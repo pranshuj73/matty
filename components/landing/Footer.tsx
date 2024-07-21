045e23c2c703006f4ef9f613369c704dd8b9c37d
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-07-21 22:27:19 +0530

    feat(footer): add privacy & tos links

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -3,11 +3,16 @@ import AnimatedShinyText from "../magicui/animated-shiny-text";
 
 export default function Footer() {
   return (
-    <footer className="mt-24">
+    <footer className="text-sm text-neutral-600/70 dark:text-neutral-400/70 flex flex-col gap-4 items-center justify-between w-full mt-24 md:flex-row">
       <AnimatedShinyText>
         Matty © {new Date().getFullYear()} ✦ By {" "}
         <Link className="border-b-2 border-dashed pb-[2px]" href={"https://pranshujha.com"} target="_blank" rel="noreferrer noopener">Pranshu Jha</Link>
       </AnimatedShinyText>
+
+      <div className="flex gap-4">
+        <Link className="border-b-2 border-dashed pb-[2px]" href="/privacy" target="_blank" rel="noreferrer noopener">Privacy Policy</Link>
+        <Link className="border-b-2 border-dashed pb-[2px]" href="/terms" target="_blank" rel="noreferrer noopener">Terms of Service</Link>
+      </div>
     </footer>
   )
 }
